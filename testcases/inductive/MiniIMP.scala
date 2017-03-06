647968c7a13331214926f90d1e72183b2502e175
Martin Wauligmann <martin@wauligmann.com>
2017-03-06 17:14:45 +0100

    MiniIMP assign fix

--- testcases/inductive/MiniIMP.scala
+++ testcases/inductive/MiniIMP.scala
@@ -1,8 +1,8 @@
 import leon.lang._
 import leon.annotation._
 
-object MiniIMP{
-  sealed abstract class Aexp
+object MiniIMP {
+  abstract class Aexp
   case class N(n: Int) extends Aexp
   case class V(x: String) extends Aexp
   case class Plus(a1: Aexp, a2: Aexp) extends Aexp
@@ -23,7 +23,6 @@ object MiniIMP{
       }
   }
   
-  
   sealed abstract class Bexp
   case class Bc(v: Boolean) extends Bexp
   case class Not(b: Bexp) extends Bexp
@@ -37,34 +36,42 @@ object MiniIMP{
     case Less(a1, a2) => aval(a1, s) < aval(a2, s)
   }
   
-  
   sealed abstract class Com
   case object Skip extends Com
   case class Assign(x: String, a: Aexp) extends Com
   case class Seq(c1: Com, c2: Com) extends Com
   case class If(b: Bexp, c1: Com, c2: Com) extends Com
   case class While(b: Bexp, c: Com) extends Com
-  
+
   @isabelle.inductive
   def big_step(c: Com, s1: String => Int, s2: String => Int): Boolean =
+
     (c, s1, s2) match {
+
       case (Skip, s1, s2) =>
         s1 == s2
+
       case (Assign(x, a), s1, s2) =>
-        s2(x) == aval(a, s1)
+        ((y: String) => { if (y == x) aval(a, s1) else s1(y) }) == s2
+
       case (Seq(c1, c2), s1, s3) =>
         exists { (s2: String => Int) =>
-          big_step(c1, s1, s2) &&
-          big_step(c2, s2, s3)
+          big_step(c1, s1, s2) && big_step(c2, s2, s3)
         }
+
       case (If(b, c1, c2), s, t) =>
-        bval(b, s) && big_step(c1, s, t) ||
-        !bval(b, s) && big_step(c2, s, t)
+        if (bval(b, s))
+          big_step(c1, s, t)
+        else
+          big_step(c2, s, t)
+
       case (While(b, c), s1, s3) =>
-        s1 == s3 && !bval(b, s1) ||
-        exists { (s2: String => Int) =>
-          bval(b, s1) && big_step(c, s1, s2) && big_step(While(b, c), s2, s3)
-        }
+        if (bval(b, s1))
+          exists { (s2: String => Int) =>
+            big_step(c, s1, s2) && big_step(While(b, c), s2, s3)
+          }
+        else
+          s1 == s3
   }
   
   def theorem(x: String, s: String => Int): Boolean = {
